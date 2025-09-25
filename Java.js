1d190eab9a47c3cb41e773cc9acdb4807030e032
Lucas de barros lima motta <lucaslimamotta2060@hotmail.com>
2025-09-24 21:55:49 -0300

    fix: Corrige o calculo do indice e reativa a funcionalidade

--- Java.js
+++ Java.js
@@ -6,3 +6,14 @@ const conselhos = [
 
 ]
 console.log("Arquivo de script carregado. Array de conselhos pronto");
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoConselhoBtn');
+
+function gerarConselho()
+{
+    const indice =Math.floor(Math.random() * (conselhos.length));
+    conselhoTexto.textContent = conselhos [indice];
+    
+}
+
+novoConselhoBtn.addEventListener('click', gerarConselho);
