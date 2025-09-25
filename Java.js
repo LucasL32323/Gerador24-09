6656a01ee9b678455225f13e9af78edc69f5154c
Lucas de barros lima motta <lucaslimamotta2060@hotmail.com>
2025-09-24 21:21:13 -0300

    Criação de upload do README.md e dos demais arquivos

---
+++ Java.js
@@ -0,0 +1,8 @@
+const conselhos = [
+    "Acredite em você mesmo" , "o primeiro passo é o mais importante" , 
+    "a persistência realiza o impossível " , "aprenda algo novo hoje" ,
+    "seja gentil com todos que encontrar"
+
+
+]
+console.log("Arquivo de script carregado. Array de conselhos pronto");
