f114fac1f0a90ca187e41cdaa0c5404a7d41d4a7
Lucas de barros lima motta <lucaslimamotta2060@hotmail.com>
2025-09-24 21:39:06 -0300

    feat: Adiciona interatividade ao botão de gerar conselho

--- Java.js
+++ Java.js
@@ -2,7 +2,20 @@ const conselhos = [
     "Acredite em você mesmo" , "o primeiro passo é o mais importante" , 
     "a persistência realiza o impossível " , "aprenda algo novo hoje" ,
     "seja gentil com todos que encontrar"
-
-
 ]
-console.log("Arquivo de script carregado. Array de conselhos pronto");
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoConselhoBtn');
+
+function gerarConselho()
+{
+    const indice =Math.floor(Math.random() * (conselhos.length +1));
+    conselhoTexto.textContent = conselhos [indice];
+    
+}
+
+novoConselhoBtn.addEventListener('click', gerarConselho);
+
+
+
+
+
